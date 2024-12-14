ba965e9d833c006ef588b12da04656dec7e765dd
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-12-14 16:48:45 +0530

    Feat: Dynamically calculate claim value using contract value.

--- src/components/rightContainer/MainContainer/MainContainerPages/claim/ClaimValue.jsx
+++ src/components/rightContainer/MainContainer/MainContainerPages/claim/ClaimValue.jsx
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Typography, Box } from "@mui/material";
 import CalculateOutlinedIcon from "@mui/icons-material/CalculateOutlined";
 import { styled } from "@mui/system";
 
-
 const CustomTextField = styled(TextField)({
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
-      border: "none", 
+      border: "none",
     },
     "&:hover fieldset": {
-      border: "none", 
+      border: "none",
     },
     "&.Mui-focused fieldset": {
-      border: "2px solid #1e83ff", 
+      border: "2px solid #1e83ff",
     },
   },
   "& .MuiInputLabel-root": {
-    color: "#000", 
+    color: "#000",
   },
 });
 
 const ClaimValue = () => {
+  const [contractValue, setContractValue] = useState(""); 
+  const [claimValue, setClaimValue] = useState("");
+
+  const handleContractValueChange = (e) => {
+    const value = e.target.value;
+    setContractValue(value);
+
+
+    const calculatedClaimValue = value ? (value * 1.5).toFixed(2) : "";
+    setClaimValue(calculatedClaimValue);
+  };
+
   return (
     <Box sx={{ width: "360px", display: "flex", flexDirection: "column" }}>
       <Box
@@ -34,29 +45,40 @@ const ClaimValue = () => {
         <CalculateOutlinedIcon style={{ fontSize: "40px", color: "#1e83ff" }} />
         <Typography variant="h6">Claim Value</Typography>
       </Box>
-      <Box sx={{ display: 'flex', paddingLeft: '50px', paddingRight: '50px' }}>
+      <Box sx={{ display: "flex", paddingLeft: "50px", paddingRight: "50px" }}>
         <Box>
+
           <CustomTextField
             label="Contract Value"
             type="number"
             fullWidth
             margin="normal"
-            sx={{ backgroundColor: '#f4f5fc' }}
+            value={contractValue}
+            onChange={handleContractValueChange}
+            sx={{ backgroundColor: "#f4f5fc" }}
             InputProps={{
               endAdornment: <Typography>USD</Typography>,
             }}
           />
+
           <CustomTextField
             label="Claim Value"
             type="number"
             fullWidth
             margin="normal"
-            sx={{ backgroundColor: '#f4f5fc' }}
+            value={claimValue}
+            sx={{ backgroundColor: "#f4f5fc" }}
             InputProps={{
+              readOnly: true,
               endAdornment: <Typography>USD</Typography>,
             }}
           />
-          <Typography variant="body2" color="#ffab45" sx={{ fontWeight: '500' }}>
+          {/* Informative Text */}
+          <Typography
+            variant="body2"
+            color="#ffab45"
+            sx={{ fontWeight: "500" }}
+          >
             150% of Contract Value
           </Typography>
         </Box>

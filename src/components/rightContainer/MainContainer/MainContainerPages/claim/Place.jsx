cb654ad03d4fffe1d56e2fd7f30a92852238e268
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-12-14 16:29:41 +0530

    Feat: Created Places DropDown.

--- src/components/rightContainer/MainContainer/MainContainerPages/claim/Place.jsx
+++ src/components/rightContainer/MainContainer/MainContainerPages/claim/Place.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Typography,
   TextField,
@@ -6,9 +6,14 @@ import {
   FormControlLabel,
   Radio,
   Box,
+  Menu,
+  MenuItem,
+  InputAdornment,
+  IconButton,
 } from "@mui/material";
 import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
 import { styled } from "@mui/system";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const CustomTextField = styled(TextField)({
   "& .MuiOutlinedInput-root": {
@@ -28,6 +33,33 @@ const CustomTextField = styled(TextField)({
 });
 
 const Place = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedPlace, setSelectedPlace] = useState(""); 
+  const [radioValue, setRadioValue] = useState("yes");
+
+  const places = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handlePlaceSelect = (place) => {
+    setSelectedPlace(place);
+    handleClose();
+  };
+
+  const handleTextFieldChange = (event) => {
+    setSelectedPlace(event.target.value);
+  };
+
+  const handleRadioChange = (event) => {
+    setRadioValue(event.target.value);
+  };
+
   return (
     <Box sx={{ width: "360px", display: "flex", flexDirection: "column" }}>
       <Box
@@ -44,14 +76,43 @@ const Place = () => {
         <Box>
           <CustomTextField
             label="Select the Place for proceedings"
+            value={selectedPlace} 
+            onChange={handleTextFieldChange} 
             fullWidth
             margin="normal"
             sx={{ backgroundColor: "#f4f5fc" }}
+            onClick={handleClick}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end">
+                    <ArrowDropDownIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            PaperProps={{
+              sx: {
+                width: anchorEl ? anchorEl.offsetWidth : "auto",
+                maxHeight: 200,
+              },
+            }}
+          >
+            {places.map((place, index) => (
+              <MenuItem key={index} onClick={() => handlePlaceSelect(place)}>
+                {place}
+              </MenuItem>
+            ))}
+          </Menu>
           <Typography>
             Is the place for the proceedings the one mentioned in the agreement?
           </Typography>
-          <RadioGroup row>
+          <RadioGroup row value={radioValue} onChange={handleRadioChange}>
             <FormControlLabel value="yes" control={<Radio />} label="Yes" />
             <FormControlLabel value="no" control={<Radio />} label="No" />
           </RadioGroup>
